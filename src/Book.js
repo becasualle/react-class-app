7507d17aa5c609f6d5162ab3efaec1253c994ebe
Mark <mark.nelubin@gmail.com>
2021-10-03 12:32:21 +0300

    Added event handler (using method + binding or using arrow function

--- src/Book.js
+++ src/Book.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react'
 
 export default class Book extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 1
+        }
+        // this.handleClick = this.handleClick.bind(this)
+    }
+
+    // state = {
+    //     count: 1
+    // }
+
+    // handleClick(e) {
+    //     console.log(e)
+    //     console.log(this.state.count)
+    // }
+
+    // arrow functions don't create their own context so we don't need to bind it
+    handleClick = () => {
+        console.log('you clicked me')
+        console.log(this.state.count)
+    }
+
     render() {
         // console.log(this.props);
         const { img, title, author } = this.props.info;
@@ -11,6 +34,7 @@ export default class Book extends Component {
                 <div>
                     <h3>Title : {title} </h3>
                     <h5>Author : {author}</h5>
+                    <button type="button" onClick={this.handleClick}>add count</button>
                 </div>
             </article>
         )
